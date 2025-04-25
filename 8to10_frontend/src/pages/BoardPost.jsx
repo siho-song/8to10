53c4912dc332718261161f84feb40d0e20753855
Woosang An <dntkd857@soongsil.ac.kr>
2025-04-25 20:38:34 +0900

    [Feat] 불필요한 코드 삭제

--- 8to10_frontend/src/pages/BoardPost.jsx
+++ 8to10_frontend/src/pages/BoardPost.jsx
@@ -1,13 +1,10 @@
-import PrivateHeader from "@/components/PrivateHeader.jsx";
 import PostView from "@/components/community/post/PostView.jsx";
-import LeftSideBar from "@/components/community/LeftSideBar.jsx";
 
 function BoardPost() {
 
     return (
         <div>
             <div className="container">
-                <LeftSideBar/>
                 <PostView />
             </div>
         </div>
